938a737481c25a56844e9064d2e13cbcb6a18c2d
MSI Laptop <henrymunprof@gmail.com>
2025-02-03 22:00:52 -0500

    fixed errors between backend files

--- web/api/auth/routes.py
+++ web/api/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_user, logout_user, login_required
-from models import User, db
+from .models import User, db
 
 api = Blueprint('api', __name__)
 
