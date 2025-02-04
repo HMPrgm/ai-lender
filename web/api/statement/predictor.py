cf1fb02671cb059006cdd882484bdc97b25b743f
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 17:44:27 -0500

    added predictor interface

---
+++ web/api/statement/predictor.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+from data.xlsx_to_np import xlsx_to_np
+from data.model_handler import get_linear_regression_model_v0, data_to_features
+from data.data_processing import consistancy_from_linear_reg_model, slope_from_linear_reg_model, history_from_nparray, balance_change
+
+def predictions_from_spreadsheet(file_path: str):
+    arr = xlsx_to_np(file_path)
+
+    X, y = data_to_features(arr)
+    model = get_linear_regression_model_v0(X,y)
+
+    slope = slope_from_linear_reg_model(model)
+    consistancy = consistancy_from_linear_reg_model(y, model.predict(X))
+    days = history_from_nparray(arr)
+    change_in_balance = balance_change(arr)
+    
+    return {
+        'slope': slope,
+        'consistancy': consistancy,
+        'days': days,
+        'change_in_balance': change_in_balance,
+    }
