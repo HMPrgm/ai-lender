92ad319418b7008913bc35fc7aa350c19dcf2b9b
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 11:52:04 -0500

    added Data to features function

--- web/api/statement/model_handler.py
+++ web/api/statement/model_handler.py
@@ -2,6 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-def get_linear_regression_model_v0(data: np.array) -> LinearRegression:
+def get_linear_regression_model_v0(X: np.array, y: np.array) -> LinearRegression:
     
+    model = LinearRegression()
+    model.fit(X,y)
+
+    return model
     
+def data_to_features(data: np.array):
+    X = np.reshape(data[:,0],(-1,1))
+    y = np.reshape(data[:,1],(-1,1))
+    return (X, y)
