a3a5a45b3448ce2c2942696a8490adf605c30e1a
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 17:41:06 -0500

    added history and change in balance metrics

--- web/api/statement/data_processing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-
-def slope_from_linear_reg_model(model: LinearRegression) -> float:
-    return model.coef_[0]
-
-def consistancy_from_linear_reg_model(y: np.array, y_pred: np.array) -> float:
-    return r2_score(y, y_pred)
