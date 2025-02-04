b90564ac2d33a687a92954bd77887997241aea79
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 01:56:30 -0500

    got nparray on test file

--- web/api/statement/xlsx_to_np.py
+++ web/api/statement/xlsx_to_np.py
@@ -8,4 +8,11 @@ TEST_FILE = "C:\\Users\\henry\\Documents\\CS Proj\\Job Materials\\Casca\\model\\
 
 df = pd.read_excel(TEST_FILE)
 
+df[date] = pd.to_datetime(df[date])
+
+first_date = df[date].min()
+df[date] = (df[date] - first_date).dt.days
+
 arr = df[[date,balance]].to_numpy()
+
+print(arr)
