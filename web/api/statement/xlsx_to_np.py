6c6f00244414aae6c92b00c0945e9852a430a86c
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 01:52:52 -0500

    converted to np array

--- web/api/statement/xlsx_to_np.py
+++ web/api/statement/xlsx_to_np.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 
-date = "date"
-balance = "balance"
+date = "Date"
+balance = "Balance"
 
 TEST_FILE = "C:\\Users\\henry\\Documents\\CS Proj\\Job Materials\\Casca\\model\\bank_statements\\Llyods Bank.xlsx"
 
 df = pd.read_excel(TEST_FILE)
 
-print(df.head())
+arr = df[[date,balance]].to_numpy()
