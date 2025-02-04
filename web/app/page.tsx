3fede5dd1615bce9f1843ee289224173beb65878
MSI Laptop <henrymunprof@gmail.com>
2025-02-03 22:14:13 -0500

    added login and register pages and got data from server to client

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    fetch('/v1/hello')
+    fetch('/auth/hello')
       .then(res => res.json())
       .then((data: MessageResponse) => {
         setMessage(data.message)
