d7daa71f1e8ca944da4bf454bac6709fc99fe899
MSI Laptop <henrymunprof@gmail.com>
2025-02-04 01:47:25 -0500

    Read excel sheet

--- web/api/statement/xlsx_to_np.py
+++ web/api/statement/xlsx_to_np.py
@@ -3,3 +3,8 @@ import pandas as pd
 date = "date"
 balance = "balance"
 
+TEST_FILE = "C:\\Users\\henry\\Documents\\CS Proj\\Job Materials\\Casca\\model\\bank_statements\\Llyods Bank.xlsx"
+
+df = pd.read_excel(TEST_FILE)
+
+print(df.head())
