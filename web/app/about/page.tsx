014c60d461adbec7236b5cd816b7b44df8495a01
MSI Laptop <henrymunprof@gmail.com>
2025-02-05 14:03:29 -0500

    added better styling

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='mb-24'>
+    <div className='mb-24 mx-2 flex flex-col items-center'>
       <h1 className='text-center text-5xl mt-8 mb-8 font-light'>About</h1>
-      <p className='mx-96 text-lg'><span className='font-medium'>Thank you for giving me this opportunity to display my skills.</span> Over the past week I designed and implemented a full-stack ML based app that can analyze and provide insights on a bank statement. This page is here to describe the architecture and technologies that were used and the reasons behind them.</p>
+      <p className='w-[42em] text-lg'><span className='font-medium'>Thank you for giving me this opportunity to display my skills.</span> Over the past week I designed and implemented a full-stack ML based app that can analyze and provide insights on a bank statement. This page is here to describe the architecture and technologies that were used and the reasons behind them.</p>
       <h2 className='text-center text-3xl mt-8 mb-4 font-light'>Model Architecture</h2>
-      <p className='mx-96 text-lg'>The first thing I did when I looked at the challenge page, was realize that I didn’t really know what a good or bad bank statement was. I did some research and found that there were many different measures of reliability. I ended up using a linear regression model as it could measure</p>
-      <ul className='mx-[24em] text-lg my-2'>
+      <p className='w-[42em] text-lg'>The first thing I did when I looked at the challenge page, was realize that I didn’t really know what a good or bad bank statement was. I did some research and found that there were many different measures of reliability. I ended up using a linear regression model as it could measure</p>
+      <ul className='w-[36em] text-lg my-2'>
         <li className='list-disc'>
           Positive change in balance over time
         </li>
@@ -15,9 +15,9 @@ export default function About() {
           Consistency in expenses and income
         </li>
       </ul>
-      <p className='mx-96 text-lg'>The model itself would input a matrix where each row represents a transaction, containing the date and balance. This matrix was created from an xlsx file converted using numpy operations. Then the Linear regression model would train on the data, taking in the date as its feature and the balance as its label. After the model was trained four data points were taken.
+      <p className='w-[42em] text-lg'>The model itself would input a matrix where each row represents a transaction, containing the date and balance. This matrix was created from an xlsx file converted using numpy operations. Then the Linear regression model would train on the data, taking in the date as its feature and the balance as its label. After the model was trained four data points were taken.
       </p>
-      <ul className='mx-[24em] text-lg my-2'>
+      <ul className='w-[36em] text-lg my-2'>
         <li className='list-disc'>
           Slope of the model - represents the average change in balance over time.
         </li>
@@ -31,10 +31,10 @@ export default function About() {
           How many Days the bank statement covers - the more history on the statement, the more reliable its data is.
         </li>
       </ul>
-      <p className='mx-96 text-lg'>
+      <p className='w-[42em] text-lg'>
         Then a score would be calculated for the bank statement based on the four points. As such the score is out of 4
       </p>
-      <ul className='mx-[24em] text-lg my-2'>
+      <ul className='w-[36em] text-lg my-2'>
         <li className='list-disc'>
           +1, the slope is positive.
 
@@ -51,13 +51,13 @@ export default function About() {
           +1, the total change in balance is positive.
         </li>
       </ul>
-      <p className='mx-96 text-lg'>This score, along with descriptions of each point would help provide insights to the bank.
+      <p className='w-[42em] text-lg'>This score, along with descriptions of each point would help provide insights to the bank.
       </p>
       <h2 className='text-center text-3xl mt-8 mb-4 font-light'>Website Design</h2>
-      <p className='mx-96 text-lg mb-2'>
+      <p className='w-[42em] text-lg mb-2'>
         The website uses Flask to create a RESTful api and a Next.js/React frontend. Flask was chosen because it provides a relatively quick setup process making it great for prototyping. Next.js/React was chosen, since it also provides a relatively quick and easy way to make beautiful websites.
       </p>
-      <p className='mx-96 text-lg'>
+      <p className='w-[42em] text-lg'>
         The app itself is more complicated than just a method to upload statements and get an analysis. Something I&apos;m relatively good at is full-stack development, as well as UI design, so I wanted to highlight that. I created a system where a loan officer at a bank can create or login to an account, see all of their past bank statement analysis, as well as upload new ones. I would go in depth more into the website, but the demo probably shows this better.
 
       </p>
