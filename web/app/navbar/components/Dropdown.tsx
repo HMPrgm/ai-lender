38a5fb471f934bbef4106cf86607c3ee649e75f0
MSI Laptop <henrymunprof@gmail.com>
2025-02-05 00:35:53 -0500

    basic home page

--- web/app/navbar/components/Dropdown.tsx
+++ web/app/navbar/components/Dropdown.tsx
@@ -52,6 +52,7 @@ export default function Dropdown() {
     ]
 
     const handleLinkClick = async (l: NavbarLink) => {
+        setIsOpen(false)
         if (l.func) {
             await l.func()
         }
@@ -63,7 +64,7 @@ export default function Dropdown() {
         <div className="relative">
             <button
                 onClick={() => setIsOpen(!isOpen)}
-                className="flex items-center gap-2 px-4 py-2 mx-2 my-2 hover:bg-gray-100 text-dark_2 transition-colors rounded-lg"
+                className="flex items-center gap-2 px-4 py-2 mx-8 my-3 hover:bg-gray-100 text-dark_2 transition-colors rounded-lg"
             >
                 Hi, {user.name}
                 <svg
@@ -83,7 +84,7 @@ export default function Dropdown() {
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -10 }}
                         transition={{ duration: 0.2 }}
-                        className="absolute right-0 mt-4 w-48 bg-white rounded-lg border py-2"
+                        className="absolute right-0 mt-4 mr-8 w-48 bg-white rounded-lg border py-2"
                     >
                         {links.map(l => <button onClick={() => handleLinkClick(l)} className='block px-4 py-2 text-dark_2 hover:text-dark_4 transition-colors'>{l.name}</button>)}
                     </motion.div>
--- web/app/navbar/components/LoggedOut.tsx
+++ web/app/navbar/components/LoggedOut.tsx
@@ -5,7 +5,7 @@ export default function LoggedOut() {
   return (
     <div className='flex'>
         <Link href='/auth/login' className='navbar-link'>Log In</Link>
-        <Link href='/auth/register' className='navbar-link'>Register</Link>
+        <Link href='/auth/register' className='navbar-link mr-4'>Register</Link>
     </div>
   )
 }
