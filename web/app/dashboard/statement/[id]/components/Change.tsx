093f6ae2c361d2268fb7b035b74dbedf4f1c58b4
MSI Laptop <henrymunprof@gmail.com>
2025-02-05 12:30:12 -0500

    imporved statement display

--- web/app/dashboard/statement/[id]/components/Change.tsx
+++ web/app/dashboard/statement/[id]/components/Change.tsx
@@ -14,7 +14,7 @@ export default function Change({change_in_balance}:{change_in_balance: number}) 
     
   return (
     <div className={change_in_balance < 0 ? 'text-red-800' : 'text-green-800'}>
-        Change in balance over period was {formatUSD(change_in_balance)}
+        Change in balance over period was {change_in_balance < 0 ? 'negative' : (change_in_balance > 0 ? 'positive' : 'zero')}
     </div>
   )
 }
--- web/app/dashboard/statement/[id]/page.tsx
+++ web/app/dashboard/statement/[id]/page.tsx
@@ -18,7 +18,7 @@ export default function StatementPage() {
 
     useEffect(() => {
         const fetchStatement = async () => {
-            
+
             try {
                 const { data } = await axios.get(
                     `/api/statement/${id}`,
@@ -45,12 +45,27 @@ export default function StatementPage() {
     return (
         <div className='flex flex-col items-center gap-4 my-4'>
             <h1 className='text-5xl font-light text-dark_1 mb-4'>{statement.title}</h1>
-            {<Outcome statement={statement}/>}
-            
-            {<Slope slope={statement.slope}/>}
-            {<Consistancy consistancy={statement.consistancy}/>}
-            {<Change change_in_balance={statement.change_in_balance}/>}
-            {<Days days={statement.days}/>}
+            <div className='grid grid-cols-2 gap-4'>
+                {<Outcome statement={statement} />}
+
+                <div className='border py-12 px-12 rounded-xl text-center'>
+                    <h2 className='text-4xl font-light mb-2'>Contributing Factors</h2>
+                    <ul className='text-left flex flex-col gap-2'>
+                        <li className=''>
+                            {<Slope slope={statement.slope} />}
+                        </li>
+                        <li>
+                            {<Consistancy consistancy={statement.consistancy} />}
+                        </li>
+                        <li>
+                            {<Change change_in_balance={statement.change_in_balance} />}
+                        </li>
+                        <li>
+                            {<Days days={statement.days} />}
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     );
